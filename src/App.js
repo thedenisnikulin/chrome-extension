ef5af5ed0e4d9aeae5e638d505d031235704d0ce
thedenisnikulin <thedenisnikulin@gmail.com>
2020-02-17 16:08:52 +0300

    have implemented better editor and notes structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
-import React from 'react';
-import './App.css';
-import NoteEditor from './components/NoteEditor';
-import NotesList from './components/NotesList';
+import React, { useState, useMemo } from 'react'
+import { createEditor, Node } from 'slate'
+import { withReact } from 'slate-react'
+import NoteEditor from './components/NoteEditor'
+import NotesList from './components/NotesList'
 
-function App() {
+const serialize = value => {
   return (
-    <div className="App">
-      <NoteEditor />
-      <NotesList />
-    </div>
-  );
+    value
+      .map(n => Node.string(n))
+      .join('\n')
+  )
 }
 
-export default App;
+const deserialize = string => {
+  return string.split('\n').map(line => {
+    return {
+      children: [{ text: line }],
+    }
+  })
+}
+
+var initItem = 'content-0';
+localStorage.setItem(initItem, 'the first note');
+
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [currentLocation, setCurrentLocation] = useState(initItem);
+  const [notes, setNotes] = useState([
+    {
+      id: 0,
+      title: 'note_0'
+    }
+  ]);
+  const [value, setValue] = useState(deserialize(localStorage.getItem(currentLocation)) || '')
+
+  const handleClick = (e, id) => {
+    e.preventDefault();
+    notes.map(note => {
+      if (note.id === id) {
+        setCurrentLocation('content-' + note.id.toString())
+      }
+    })
+  }
+
+  const addNote = (e) => {
+    e.preventDefault();
+    let newId;
+    try {
+      newId = notes[notes.length-1].id + 1;
+    } catch (error) {
+      newId = 0;
+    }
+    const newNote = {
+      id: newId || 0,
+      title: 'note_' + newId.toString(),
+    }
+    setNotes([...notes, newNote]);
+    localStorage.setItem('content-'+ newNote.id.toString(), '')
+  }
+
+  return (
+    <div>
+      <NoteEditor 
+        editor={editor}
+        value={value}
+        setValue={setValue}
+        serialize={serialize}
+        storageLocation={currentLocation}
+      />
+      <NotesList 
+        notes={notes}
+        handleClick={handleClick}
+        addNote={addNote}
+      />
+    </div>
+  )
+}
+
+export default App
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-function NotesList() {
+function NotesList(props) {
+	return(
+		<div>
+            {props.notes.map(note =>
+                <Note 
+                    handleClick={props.handleClick}
+                    note={note} 
+                />
+            )}
+            <input value='add note' type='button' onClick={(e) => props.addNote(e)} />
+		</div>
+	);
+}
+
+function Note(props) {
     return(
-        <div className='notes-list'>
-
+        <div>
+            {props.note.title}
+            <input value='show' type='button' 
+                onClick={(e) => props.handleClick(e, props.note.id)} />
         </div>
     );
 }
 
-export default NotesList;
+export default NotesList
